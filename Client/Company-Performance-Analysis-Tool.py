ff680178f6c00adfe066565c851bc4278445712c
Bolocan Vlad-Constantin <vladconstantin209@gmail.com>
2024-12-10 19:12:57 +0200

    Prediction model interface
    Client app needs to import whatever class the object that holds the data has, so I made an interface to keep behaviour clear.

--- Client/Company-Performance-Analysis-Tool.py
+++ Client/Company-Performance-Analysis-Tool.py
@@ -1,29 +1,26 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
+from ..Model.PredictionModel import PredictionModel
 
 app = Flask(__name__)
 
-# model = joblib.load('../Model/model_obj.pkl')
+model: PredictionModel = joblib.load('../Model/model_obj.pkl')
 prediction = None
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-def screener(data):
-    print("PING")
-    return render_template('screener.html', data=data)
-
 @app.route('/post/<string:company>')
 def get_by_name(company):
-    # prediction = model.predict(model.find(company))
-    return screener(prediction)
+    cui = model.find(company)
+    return get_by_id(cui)
 
 @app.route('/post/<int:cui>')
 def get_by_id(cui):
-    # prediction = model.predict(cui)
-    return screener(prediction)
+    prediction = model.predict(cui)
+    return render_template('screener.html', data=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Client/static/index.js
+++ Client/static/index.js
@@ -1,9 +1,6 @@
 let button = document.getElementById('search_button')
 let field = document.getElementById('search_field')
 button.addEventListener('click', function () {
-    let baseAddress = window.location.pathname
     let route = 'post/'.concat(field.value)
-    route = baseAddress.concat(route)
-    window.alert(route)
     window.location.assign(route)
 })
