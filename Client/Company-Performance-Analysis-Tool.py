d7ba1bdb27c6fffec36956b28f82de64c571226e
Bolocan Vlad-Constantin <vladconstantin209@gmail.com>
2024-12-12 18:35:12 +0200

    Basic page routing
    Can search a company and pass variable form to html

--- Client/Company-Performance-Analysis-Tool.py
+++ Client/Company-Performance-Analysis-Tool.py
@@ -1,26 +1,37 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
-from ..Model.PredictionModel import PredictionModel
+import scipy as sp
 
 app = Flask(__name__)
 
-model: PredictionModel = joblib.load('../Model/model_obj.pkl')
-prediction = None
+class PredictionModel:
+    def find(self, name: str) -> int:
+        return 0
+
+    def predict(self, id: int):
+        data = {
+            'name': 'Enel'
+        }
+        return data
+
+model = PredictionModel()
+
+# model = joblib.load('../Model/model_obj.pkl')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/post/<string:company>')
-def get_by_name(company):
+@app.route('/predict/<string:company>')
+def get_by_name(company: str):
     cui = model.find(company)
     return get_by_id(cui)
 
-@app.route('/post/<int:cui>')
-def get_by_id(cui):
+@app.route('/predict/<int:cui>')
+def get_by_id(cui: int):
     prediction = model.predict(cui)
-    return render_template('screener.html', data=prediction)
+    return render_template('screener.html', **prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Model/PredictionModel.py
+++ Model/PredictionModel.py
@@ -1,17 +1,7 @@
 import joblib
 import numpy as np
 
-# Interface used by client to fetch predictions. Feel free to add any other members, don't remove find and predict.
-class PredictionModel:
-    def find(name: str):
-        # Find company's CUI by name, if possible
-        pass
-    
-    def predict(cui: int):
-        # Return a packet with all relevant data about the company. Any datastructure you want.
-        pass
-
-model = PredictionModel() # TODO train the model
+model = None # TODO train the model
 
 # Dump the trained model into the model_obj.pkl file. It will conserve it as binary data.
 joblib.dump(model, 'model_obj.pkl')
